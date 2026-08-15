6eff37fea63f4406f1262521bbea7a4a53cae132
Rohit Kr Thakur <116970753+rohitkrthakur@users.noreply.github.com>
2026-08-15 21:12:55 +0530

    how input works

---
+++ 01_basics/input/main.py
@@ -0,0 +1,34 @@
+# name = input("what is your name")
+
+# print(f"you name is {name}")
+
+# note input always return string
+
+
+# name = input("Enter your name :")
+# age = int(input("Enter your age :"))
+
+# print(f"you name is {name}")
+# print(f"you age is {age}")
+
+
+# input calculation
+
+# num1 = int(input("enter first number :"))
+# num2 = int(input("enter second number :"))
+
+# result = num1 + num2
+
+# print(result)
+
+
+
+# without int
+
+# num1 = (input("enter first number :"))
+# num2 = (input("enter second number :"))
+
+# result = num1 + num2
+
+# print(result)
+
