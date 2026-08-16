c75659d002c8f76f22180db15a55c8a111a2ac7e
Rohit Kr Thakur <116970753+rohitkrthakur@users.noreply.github.com>
2026-08-16 21:04:53 +0530

    arithmetic

---
+++ Arithmeticoperators/maiin.py
@@ -0,0 +1,24 @@
+a = 10
+b=3
+
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b)
+# print(a%b)
+
+
+
+# print(2*2*2*2)
+
+
+# Comparison operators
+
+
+# print(a == b)   # False
+# print(a != b)   # True
+# print(a > b)    # True
+# print(a < b)    # False
+# print(a >= b)   # True
+# print(a <= b) 
