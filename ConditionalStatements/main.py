fe654a42eb634249e769fc4a7319631c7ea2bd6b
Rohit Kr Thakur <116970753+rohitkrthakur@users.noreply.github.com>
2026-08-17 16:02:54 +0530

    conditions

---
+++ ConditionalStatements/main.py
@@ -0,0 +1,48 @@
+
+# age = 24
+
+# if age>18:
+#     print("you are adult")
+
+
+
+# age = 16
+
+# if age>=18:
+#     print("you are adult")
+# else:
+#      print("you are not adult") 
+
+
+# marks = 75
+
+# if marks >= 90:
+#     print("A")
+# elif marks >= 60:
+#     print("B")
+# else:
+#     print("C")
+
+# age = 25
+# if age >= 18 and age<=60:
+#     print("Working age")
+
+
+# age = 18
+# has_license = True
+
+# if age >= 18:
+#     if has_license:
+#         print("You can drive")
+#     else:
+#         print("Get a license")
+# else:
+#     print("Too young")
+
+
+# name = ""
+
+# if name:
+#     print("Name exists")
+# else:
+#     print("Name is empty")
