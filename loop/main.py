126ae32eb2113d8addae03f790edf8e9bc13d49e
Rohit Kr Thakur <116970753+rohitkrthakur@users.noreply.github.com>
2026-08-18 06:29:31 +0530

    loops

---
+++ loop/main.py
@@ -0,0 +1,31 @@
+# for i in range(5):
+#     print(i)
+
+# for i in range(1,6):
+#     print(i)
+
+
+
+# names = ["rohit", "ritik","ujjawal"]
+# for name in names:
+#     print(name)
+
+
+# count = 1
+# while count <=5:
+#     print(count)
+#     count += 1
+
+
+# for i in range(10):
+#     if i ==5:
+#         break
+
+#     print(i)
+
+
+# continue  
+# for i in range(5):
+#     if i ==2:
+#         continue
+#     print(i)
